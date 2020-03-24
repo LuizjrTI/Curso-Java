326b20659af4465ac5d7ec52554d2dcaf62f719b
Your Name <you@example.com>
2020-03-24 16:38:47 -0300

    Primeiro codigo Java

---
+++ src/fundamentos/primeiroPrograma.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class primeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
