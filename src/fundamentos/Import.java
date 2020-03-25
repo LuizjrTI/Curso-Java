3f59ed7eb172e51bae005bc9f3cfd21078416023
Your Name <you@example.com>
2020-03-25 19:25:53 -0300

    Aula21

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author Luiz Junior
+ * @category Aula21
+ * 
+ * aula sobre import's
+ *
+ */
+
+
+public class Import {
+	
+	public static void main(String[] args) {
+		
+		String s = "Bom Dia !!";
+		System.out.println(s);
+		
+		Date date = new Date();
+		System.out.println(date);
+				
+	}
+	
+
+}
