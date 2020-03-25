1a9165690532de7985de3d9ee6319ff9a7f0d74e
Your Name <you@example.com>
2020-03-25 15:44:45 -0300

    Aula19

---
+++ src/fundamentos/tiposPrimitivos.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+public class tiposPrimitivos {
+	
+	public static void main(String[] args) {
+		
+		// informações do funcionario
+		
+		// tipos numericos inteiros
+		 byte anosDeEmpresa = 23;
+		 short numeroDeVoos = 542;
+		 int id = 56789;
+		 long pontosAcumulados = 3_234_845_223L; // necessita por um L/l no final do numero pois passou do range do Long
+		 
+		// tipos numericos reais
+		 float salario = 11_445.44F;
+		 double vendasAcumuladas = 2_991_797_103.01;
+		 
+		//tipos booleano
+		 boolean estaDeFerias = false;
+		
+	    //tipo caractere
+		 char status = 'A';
+		 
+		 
+		 
+	
+	}
+
+}
