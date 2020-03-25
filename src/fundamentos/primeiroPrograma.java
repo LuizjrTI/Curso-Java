f55e9d9b781cd4d2fd09c089c17d27fef8eecad9
Your Name <you@example.com>
2020-03-25 12:15:57 -0300

    Atualização da Aula 13

--- src/fundamentos/primeiroPrograma.java
+++ src/fundamentos/primeiroPrograma.java
@@ -3,6 +3,7 @@ package fundamentos;
 public class primeiroPrograma {
 	
 	// tudo começa a partir de um main!!!
+	// CRLT + / -> COMENTA A LINHA 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
