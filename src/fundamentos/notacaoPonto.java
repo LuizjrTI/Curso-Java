5051201a826aff5d9f410b17627ed98de2dfac94
Your Name <you@example.com>
2020-03-25 16:02:31 -0300

    Aula20 Notacao Ponto

---
+++ src/fundamentos/notacaoPonto.java
@@ -0,0 +1,34 @@
+package fundamentos;
+
+public class notacaoPonto {
+	
+	public static void main(String[] args) {
+		
+		
+		String s = "Bom dia x";
+		s = s.replace("x","Senhor");
+		s = s.toUpperCase();
+		s = s.concat("!!!");
+		System.out.println(s);
+
+		String x = "leo".toUpperCase();
+		System.out.println(x);
+		
+		String y = "Bom dia X"
+				.replace("X", x)
+				.toUpperCase()
+				.concat("!!!");
+		
+		System.out.println(y);
+		
+		
+		//Tipos primitivos não te o operador "."
+		
+		int a = 3;
+		System.out.println(a);
+		
+		
+		
+	}
+
+}
