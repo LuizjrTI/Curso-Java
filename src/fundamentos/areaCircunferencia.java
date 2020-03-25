36c9cc7edf211c260d1db129f710291413d3f319
Your Name <you@example.com>
2020-03-25 12:50:55 -0300

    Criação da classe areaCircuferencia Aula14

---
+++ src/fundamentos/areaCircunferencia.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+/**
+ * 
+ * @author Luiz Junior
+ *
+ */
+public class areaCircunferencia {
+
+	public static void main(String[] args) {
+		
+//		System.out.println(2+3);
+		double raio = 4.5;
+		final double PI = 3.14159; //constante
+		
+//		double resultado = PI*raio*raio;
+		
+		System.out.println("A área do circulo é: " + (PI*raio*raio));
+		
+				
+	}
+}
