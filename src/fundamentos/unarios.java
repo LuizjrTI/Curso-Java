c36cb1a5d3613d04b06322fd7f89dbca4cf2b783
Your Name <you@example.com>
2020-03-27 15:03:33 -0300

    Aula42 - Unarios / Posfix e Prefix

---
+++ src/fundamentos/unarios.java
@@ -0,0 +1,37 @@
+package fundamentos;
+
+public class unarios {
+	
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		a++;
+		a--;
+		
+		++b;
+		--b;
+		
+		/* No caso de operadores Unarios:
+		 * 
+		 * Posfix (a++) -> Primeiro exibisse o valor da variavel e depois incrementa.
+		 * 
+		 * Prefix (++a) -> Primeiro incrementa o valor e exibe depois
+		 *  
+		 */
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		System.out.println("Mini Desafio");
+		System.out.println(++a == b--);
+		System.out.println(a == b);
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
+	}
+
+}
