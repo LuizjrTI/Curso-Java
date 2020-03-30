ebc1f8afdcb5727ff2876bf66d791cd4a6a3f552
Your Name <you@example.com>
2020-03-30 16:50:20 -0300

    Atualização de identação

--- src/fundamentos/atribuicao.java
+++ src/fundamentos/atribuicao.java
@@ -1,24 +1,24 @@
 package fundamentos;
 
 public class atribuicao {
-	
+
 	public static void main(String[] args) {
-		
+
 		int a = 3;
 		int b = a;
 		int c = a + b;
-		
-		c += b; //c = c + b;
-		c -= b; //c = c - b;
-		c *= b; //c = c * b;
-		c /= b; //c = c / b;
-		
+
+		c += b; // c = c + b;
+		c -= b; // c = c - b;
+		c *= b; // c = c * b;
+		c /= b; // c = c / b;
+
 		System.out.println(c);
-		
-		c %= b; //c = c % b; Operação de modulo
-		
+
+		c %= b; // c = c % b; Operação de modulo
+
 		System.out.println(c);
-		
+
 	}
 
 }
