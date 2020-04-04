2ceb7b796cafb564444cd256518eabdd9620bad7
Your Name <you@example.com>
2020-04-04 12:32:01 -0300

    Aula sobre o uso do If e Else

--- src/controle/ifElse.java
+++ src/controle/ifElse.java
@@ -12,10 +12,14 @@ public class ifElse {
 		int numero = Integer.parseInt(valor);
 
 		if (numero % 2 == 0) {
+			
+			JOptionPane.showMessageDialog(null, "Número Par");
+			
 
 			System.out.println("Número Par");
 		} else {
-			System.out.println("Número Imparr");
+			JOptionPane.showMessageDialog(null, "Número Impar");
+			System.out.println("Número Impar");
 		}
 
 	}
