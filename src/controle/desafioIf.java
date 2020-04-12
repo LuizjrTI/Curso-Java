0c8fb1337e1a08a1434e84d5e52f88158966383c
Your Name <you@example.com>
2020-04-11 22:27:44 -0300

    Ultima atualização

--- src/controle/desafioIf.java
+++ src/controle/desafioIf.java
@@ -9,7 +9,7 @@ public class desafioIf {
 
 		if (nota >= 9.0) {
 			System.out.println("Quadro de Honra!");
-			System.out.println("Você é fera");
+			System.out.println("Vocï¿½ ï¿½ fera");
 		}
 
 	}
